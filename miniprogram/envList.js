20050d038ef0687dd78adbae027e89acfb55845c
ljz1341970144 <1341970144@qq.com>
2021-07-12 16:47:33 +0800

    first commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-7g8o4xrgdcc952da","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-7g8o4xrgdcc952da --n quickstartFunctions --r --project "C:\Users\LX\Desktop\MyFirstApp" --report
